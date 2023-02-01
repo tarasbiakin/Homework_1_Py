3a208eba9ef9de7cb8ad1ce799b0349e48f4d753
tarasbiakin <115660593+tarasbiakin@users.noreply.github.com>
2023-02-01 17:11:38 +0300

    init commit

---
+++ case_2.py
@@ -0,0 +1,13 @@
+# Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+# таблицы истинности
+
+x = int(input('Введите число x '))
+y = int(input('Введите число y '))
+z = int(input('Введите число z '))
+
+for x in range(2):
+        for y in range(2):
+            for z in range(2):
+                print(not (x or y or z) == (not x and not y and not z))
+                print(x, y, z)
+
